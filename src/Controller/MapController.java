29b32eb3be81065471d6be79ba04049739b63c3b
stithwilliam <stithwilliam@gmail.com>
2015-11-03 00:51:12 -0500

    Fixed tiles and working on Terminal

--- src/Controller/MapController.java
+++ src/Controller/MapController.java
@@ -5,12 +5,16 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 
 import java.util.HashMap;
 
@@ -19,8 +23,6 @@ import java.util.HashMap;
  */
 public class MapController {
 
-    @FXML
-    private ScrollPane scrollPane;
     @FXML
     private GridPane gridPane;
     @FXML
@@ -35,6 +37,14 @@ public class MapController {
     private Button moveBtn;
     @FXML
     private Button atkBtn;
+    @FXML
+    private Button skillBtn;
+    @FXML
+    private Button prevBtn;
+    @FXML
+    private Button nextBtn;
+    @FXML
+    private FlowPane terminal;
 
     private Map map;
     private Fighter fighter;
@@ -61,8 +71,12 @@ public class MapController {
 
         //test code
         Fighter theKing = new Fighter(Hero.LIZARDKING, 7, 4, false);
+        Fighter chaos = new Fighter(Hero.CHAOS, 2, 2, true);
         StackPane starter = map.getMapTile(7, 4).getStackPane();
         starter.getChildren().add(new ImageView(Hero.LIZARDKING.imagePath()));
+        StackPane pane = map.getMapTile(2, 2).getStackPane();
+        pane.getChildren().add(new ImageView(Hero.CHAOS.imagePath()));
+        map.addFighter(chaos);
         map.addFighter(theKing);
         fighter = theKing;
         //test code \\
@@ -141,5 +155,15 @@ public class MapController {
         leftRect.setOnMouseClicked(this::setRectClicked);
         moveBtn.setOnAction(this::setMoveBtn);
         atkBtn.setOnAction(this::setAtkBtn);
+        Label toConsole = new Label();
+        toConsole.setText("I am a console hear me roar! ");
+        terminal.getChildren().add(consoleLabel(toConsole));
+    }
+
+    private Label consoleLabel(Label l) {
+        l.setTextFill(Paint.valueOf("lime"));
+        l.setFont(Font.font("System Regular", 16));
+        System.out.println("label: " + l.getText());
+        return l;
     }
 }
